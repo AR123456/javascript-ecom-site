2ab848f2f1300b3a4e1ecbb0589e6010b38a4007
AR123456 <coloradoz99@gmail.com>
2020-02-08 10:20:16 -0700

    express requests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi there!');
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`App is listening of port "http://localhost:${port}"`)
+);

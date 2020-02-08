1de62b903bec8161f4425f46aa41d346a6701b0a
AR123456 <coloradoz99@gmail.com>
2020-02-08 10:20:59 -0700

    simple html in template literal

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.send(
+    `<div> 
+  <form>
+  <input placeholder="email"/>
+  <input placeholder="password"/>
+  <input placeholder="password confirmation"/>
+  <button>Sign Up</button>
+  </form>
+  </div>`
+  );
+});
 
 app.listen(port, () =>
   console.log(`App is listening of port "http://localhost:${port}"`)

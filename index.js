5cf7d3523cace20dc3c5c52f57439c75fed57235
AR123456 <coloradoz99@gmail.com>
2020-02-09 10:31:30 -0700

    form submit

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send(
     `<div> 
-  <form>
-  <input placeholder="email"/>
-  <input placeholder="password"/>
-  <input placeholder="password confirmation"/>
+  <form method="POST">
+  <input name="email" placeholder="email"/>
+  <input name="password" placeholder="password"/>
+  <input name="passwordConfirmation" placeholder="password confirmation"/>
   <button>Sign Up</button>
   </form>
   </div>`
   );
 });
-
+app.post("/", (req, res) => {
+  res.send(
+    `<div>
+  <h1>Account Created </h1>
+  </div>`
+  );
+});
 app.listen(port, () =>
   console.log(`App is listening of port "http://localhost:${port}"`)
 );

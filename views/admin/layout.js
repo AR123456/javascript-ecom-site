c0e5a734b5208fc26909fb546415b4fb55b1183b
AR123456 <coloradoz99@gmail.com>
2020-02-24 05:49:53 -0700

    layout file

---
+++ views/admin/layout.js
@@ -0,0 +1,14 @@
+module.exports = ({ content }) => {
+  //string that represents the content of the web page
+  //  this is how we will inject singin and signup ect in to body
+  return `
+ <!DOCTYPE html>
+    <html>
+        <head>
+        </head>
+            <body>
+            ${content}
+            </body>
+     </html>
+  `;
+};

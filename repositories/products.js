94d4ffc6c94878c10080f84a80c339519032a7b1
AR123456 <coloradoz99@gmail.com>
2020-03-03 06:08:35 -0700

    product repo

---
+++ repositories/products.js
@@ -0,0 +1,14 @@
+// home of the methods for the handleing of products
+
+// will need to get list of all products, find product by id, find  one prodcut with given filter, create product with given attributes, update product with given ID with given attributes , delete product with given id , generate random id, and write all products to json file
+
+// getAll get list of all products
+//getOne find product by id
+//getOneBy find  one prodcut with given filter
+//create create product with given attributes
+//update  update product with given ID with given attributes
+//delete  delete product with given id
+//randomId  generate random id
+//writeAll  write all products to json file
+
+// since the methods for products and users are so similar will use a master class to keep it DRY

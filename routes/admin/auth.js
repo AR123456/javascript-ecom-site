c174789b877a0869746c004836d326cc1961d524
AR123456 <coloradoz99@gmail.com>
2020-04-03 04:40:40 -0600

    success redirect

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -25,7 +25,8 @@ router.post(
     const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
     req.session.userId = user.id;
-    res.send("Account created!!!");
+    // res.send("Account created!!!");
+    res.redirect("/admin/products");
   }
 );
 
@@ -46,7 +47,8 @@ router.post(
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });
     req.session.userId = user.id;
-    res.send("You are signed in!!!");
+    // res.send("You are signed in!!!");
+    res.redirect("/admin/products");
   }
 );
 

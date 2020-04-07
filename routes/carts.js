cbc36561a543d2e06b8fef2c44bb05a6c4b81fc3
AR123456 <coloradoz99@gmail.com>
2020-04-07 06:39:07 -0600

    cart boilerplate

---
+++ routes/carts.js
@@ -0,0 +1,16 @@
+// route handlers for products for users
+const express = require("express");
+// const cartsRepo = require("../repositories/carts");
+
+const router = express.Router();
+
+// add item to cart - form submit - post request on submit 
+// recive post request 
+
+// show the stuff in the cart 
+// recive get 
+
+//recive post 
+
+
+module.exports = router;

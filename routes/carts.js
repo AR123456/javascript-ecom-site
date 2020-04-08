c20c19cc59da5872db60071db2f3bc98fd2f20f5
AR123456 <coloradoz99@gmail.com>
2020-04-08 06:30:20 -0600

    create cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,13 +1,26 @@
 // route handlers for products for users
 const express = require("express");
-// const cartsRepo = require("../repositories/carts");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
 
 // add item to cart - form submit - post request on submit
 // recive post request
-router.post("/cart/products", (req, res) => {
-  console.log(req.body.productId);
+router.post("/cart/products", async (req, res) => {
+  // figure out if there is a cart
+  let cart;
+  if (!req.session.cartId) {
+    // create cart
+    // store cart id on the req.session.cartId
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    // we have cart so get it from repo
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  console.log(cart);
+  // increment quantity for existing product
+  // or add new product to cart
   res.send("product added ");
 });
 // show the stuff in the cart

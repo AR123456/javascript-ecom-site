7687679e4a6a3956e3134ae6b7c1d9ed2898a232
AR123456 <coloradoz99@gmail.com>
2020-04-08 06:30:54 -0600

    add items cart

--- routes/carts.js
+++ routes/carts.js
@@ -19,8 +19,20 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
   console.log(cart);
-  // increment quantity for existing product
-  // or add new product to cart
+  // check to see if product is in cart
+  const existingItem = cart.items.find(item => item.id === req.body.productId);
+
+  if (existingItem) {
+    //increment quantity for existing product
+    existingItem.quantity++;
+  } else {
+    // or add new product to cart
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items
+  });
+
   res.send("product added ");
 });
 // show the stuff in the cart

e8067c89804dfdd585766579e437916461a2a2be
AR123456 <coloradoz99@gmail.com>
2022-02-15 07:15:20 -0700

    ecom 2022 redo

--- repositories/carts.js
+++
@@ -1,8 +0,0 @@
-const Repository = require("./repository");
-
-// make use of the generic repository in products
-class CartsRepository extends Repository {
-  // will add more here
-}
-// create a new instance of productsRepository and export it
-module.exports = new CartsRepository("carts.json");

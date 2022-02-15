06904a45ce6f0d33adcf2634c6d758aa9e1519ff
AR123456 <coloradoz99@gmail.com>
2022-02-15 07:17:56 -0700

    generate some simple HTML

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
-
-app.get("/", (req, res) => res.send("Hello World!"));
+// when we go to the root show this HTML
+app.get("/", (req, res) => {
+  // put a tempate string into the res.send
+  // if the browser sees something that looks like HTML it will render it as such
+  res.send(
+    `<div> 
+  <form>
+  <input placeholder="email"/>
+  <input placeholder="password"/>
+  <input placeholder="password confirmation"/>
+  <button>Sign Up</button>
+  </form>
+  </div>`
+  );
+});
 
 app.listen(port, () =>
-  //  in this HTTP request there are 4 things the
-  //  host - local host - this could be anything like google or amazon
-  //  port - 3000  - mosts of the time in real world the default is used
-  //  the path - /  - where on the sever to go, default is slash
-  //  were are doing app.get so the Method is "GET", when we hit enter key this happens. this is the intent of the req
-
-  // the GET request is registering the request with the router object, the router then passes off to the callback function
-
   console.log(`App is listening of port "http://localhost:${port}"`)
 );

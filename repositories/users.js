7c8dbef5b95357d45d3ca989ad138283d05cd545
AR123456 <coloradoz99@gmail.com>
2022-02-19 07:31:52 -0700

    updating records

--- repositories/users.js
+++ repositories/users.js
@@ -43,19 +43,39 @@ class UsersRepository {
   }
   async delete(id) {
     const records = await this.getAll();
-    //filter retains elements that return true, filters out false
     const filteredRecords = records.filter((record) => record.id !== id);
-    // pass filteredRecords into the write all funciton to store in the file on the local computer
+    // pass filteredRecords into the write all funciton to store in the file
     await this.writeAll(filteredRecords);
+  }
+  async update(id, attrs) {
+    // get all records then find the one we care about
+    // will need to entire list later to put back into the json
+    const records = await this.getAll();
+    // find the id that was passed into the update function
+    const record = records.find((record) => record.id === id);
+    if (!record) {
+      // this is one of many errors that could occur, this is a big one though. At some point have to decide the level of error catching that is appropriate for the app 
+      throw new Error(`No record found with id of ${id}`);
+    }
+
+    // https://www.geeksforgeeks.org/object-assign-javascript/
+    // will take all of the differrent properties and key value paris inside the attrs object and copy thme one by one into the records object .   IE take all the properties from adders and assing them or copy them over to record
+    // record ==={email:"test@test.com"}
+    // attrs ==={password:"mypassword"}
+    Object.assign(record, attrs);
+    // record = {email:"test@test.com", password: "mypassword"}
+    /////
+    //write back to file - this is all the users not just the updated one 
+    await this.writeAll(records);
   }
 }
 
 // testing on the fly
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  // await repo.create({ email: "test@test.com", password: "password" });
-  // const users = await repo.getAll();
-  // test delete
-  await repo.delete("2c592313");
+  // create a user
+  // await repo.create({ email: "test2@test2.com" });
+  // call update and add password
+  await repo.update("cdbdf6f6", { password: "password2" });
 };
 test();

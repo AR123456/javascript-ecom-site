c6ada4a5193e97928aab57ea34e41ecb42ee839c
AR123456 <coloradoz99@gmail.com>
2022-02-19 07:33:14 -0700

    get on by a filter

--- repositories/users.js
+++ repositories/users.js
@@ -36,46 +36,58 @@ class UsersRepository {
     return crypto.randomBytes(4).toString("hex");
   }
   async getOne(id) {
-    // get records
     const records = await this.getAll();
-    // use find array method find first record with an id property === the value that was passed in
     return records.find((record) => record.id === id);
   }
   async delete(id) {
     const records = await this.getAll();
     const filteredRecords = records.filter((record) => record.id !== id);
-    // pass filteredRecords into the write all funciton to store in the file
     await this.writeAll(filteredRecords);
   }
   async update(id, attrs) {
-    // get all records then find the one we care about
-    // will need to entire list later to put back into the json
     const records = await this.getAll();
-    // find the id that was passed into the update function
     const record = records.find((record) => record.id === id);
     if (!record) {
-      // this is one of many errors that could occur, this is a big one though. At some point have to decide the level of error catching that is appropriate for the app 
       throw new Error(`No record found with id of ${id}`);
     }
-
-    // https://www.geeksforgeeks.org/object-assign-javascript/
-    // will take all of the differrent properties and key value paris inside the attrs object and copy thme one by one into the records object .   IE take all the properties from adders and assing them or copy them over to record
-    // record ==={email:"test@test.com"}
-    // attrs ==={password:"mypassword"}
     Object.assign(record, attrs);
-    // record = {email:"test@test.com", password: "mypassword"}
-    /////
-    //write back to file - this is all the users not just the updated one 
     await this.writeAll(records);
+  }
+  // get one by a filter - filters object
+  // https://www.w3schools.com/js/js_loop_forof.asp
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+  async getOneBy(filters) {
+    //get the current copy of all records
+    const records = await this.getAll();
+    // for of is for arrays
+    for (let record of records) {
+      // a temporary variable
+      let found = true;
+      // iterate over filters object and for ever key value pair look for match on the keys value
+      // use for in for an object
+      for (let key in filters) {
+        // if not the same update found to false
+        // look for the value of the object at that particular key looks lke this  filters[key];
+        if (record[key] !== filters[key]) {
+          // return what is true
+          found = false;
+        }
+      }
+      if (found === true) {
+        return record;
+      }
+    }
   }
 }
 
 // testing on the fly
 const test = async () => {
   const repo = new UsersRepository("users.json");
-  // create a user
-  // await repo.create({ email: "test2@test2.com" });
-  // call update and add password
-  await repo.update("cdbdf6f6", { password: "password2" });
+  // get one using email as a filter
+  const user = await repo.getOneBy({
+    // this is filter being passed into getOneBy
+    email: "test2@test2.com",
+  });
+  console.log(user);
 };
 test();

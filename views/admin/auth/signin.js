441b5036991aca44b16bf491af500dc6457208b6
AR123456 <coloradoz99@gmail.com>
2022-02-22 05:21:18 -0700

    build layout file

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,11 +1,15 @@
-// this function will be used (imported) in auth.js to render this HTML
-// in the res.send once user has signed in
+const layout = require("../layout");
 module.exports = () => {
-  return `<div> 
-    <form method="POST">
+  // return layout with object of content with the string of the html string
+  return layout({
+    content: `
+    <div> 
+  <form method="POST">
   <input name="email" placeholder="email"/>
   <input name="password" placeholder="password"/>
   <button>Sign In</button>
   </form>
-  </div>`;
+  </div>
+  `
+  });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,16 +1,19 @@
+const layout = require("../layout");
 // create a function to generate the HTML and export it
-// pass in the req object so that it renders correctly(coming from route handler)
+// pass in the req object
 module.exports = ({ req }) => {
   //
-  return `
+  return layout({
+    content: `
   <div> 
   Your Id is: ${req.session.userId}
-<form method="POST">
-<input name="email" placeholder="email"/>
-<input name="password" placeholder="password"/>
-<input name="passwordConfirmation" placeholder="password confirmation"/>
-<button>Sign Up</button>
-</form>
-</div>
-  `;
+  <form method="POST">
+  <input name="email" placeholder="email"/>
+  <input name="password" placeholder="password"/>
+  <input name="passwordConfirmation" placeholder="password confirmation"/>
+  <button>Sign Up</button>
+  </form>
+  </div>
+  `
+  });
 };

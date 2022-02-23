09f0685a0a5240676768416e479af6c758298982
AR123456 <coloradoz99@gmail.com>
2022-02-23 05:54:04 -0700

    build layout file

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,9 +1,15 @@
+const layout = require("../layout");
 module.exports = () => {
-  return `<div> 
+  // return layout with object of content with the string of the html string
+  return layout({
+    content: `
+    <div> 
   <form method="POST">
   <input name="email" placeholder="email"/>
   <input name="password" placeholder="password"/>
   <button>Sign In</button>
   </form>
-  </div>`;
+  </div>
+  `
+  });
 };
--- views/admin/layout.js
+++ views/admin/layout.js
@@ -1,4 +1,6 @@
 module.exports = ({ content }) => {
+  //string that represents the content of the web page
+  //  this is how we will inject signin and signup ect in to body
   return `
  <!DOCTYPE html>
     <html>

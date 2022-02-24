57dc900365a4522f2f809756d2c17ec8223a0c32
AR123456 <coloradoz99@gmail.com>
2022-02-24 16:03:53 -0700

    adding styling

--- views/helpers.js
+++ views/helpers.js
@@ -1,6 +1,3 @@
-//array of helper objects can be imported where needed and needed oject used as function
-// using in singin and signup js
-
 module.exports = {
   getError(errors, prop) {
     try {
@@ -8,5 +5,5 @@ module.exports = {
     } catch (err) {
       return "";
     }
-  },
+  }
 };

c20b8efdbff764c227efec2a1498e12fe61e8a1f
AR123456 <coloradoz99@gmail.com>
2022-02-26 07:20:33 -0700

    multer middleware for files

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,12 +1,16 @@
 // import express
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 // get validators
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+// multer needs a location to put the file while it is being uploaded, while we are waiting to
+// do something with it in the request handler
+const upload = multer({ storage: multer.memoryStorage() });
 
 ///1 list products
 router.get("/admin/products", (req, res) => {
@@ -21,19 +25,22 @@ router.get("/admin/products/new", (req, res) => {
 });
 ///3 submit create product form
 // 3 params route, validators and the ususal req,res
-router.post("/admin/products/new", [requireTitle, requireTitle], (req, res) => {
-  //
-  const errors = validationResult(req);
-  // console.log(errors);
-  // console.log(req.body);
-  req.on("data", data => {
-    // console.log(data.toString());
-  });
-  res.send("submitted");
-});
+router.post(
+  "/admin/products/new",
+  [requireTitle, requireTitle],
+  // here image is coming from the form input name attribute
+  upload.single("image"),
+  (req, res) => {
+    //
+    const errors = validationResult(req);
+    console.log(req.file);
+
+    res.send("submitted");
+  }
+);
 ///4 show edit product route
 /// 5 submit edited product
 /// 6 delete product
-// export the moule
+// export the module
 module.exports = router;
 // go to index.js and import this router

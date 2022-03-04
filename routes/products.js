fdc885be22bbec440b3137d0ab6146035663bf2f
AR123456 <coloradoz99@gmail.com>
2022-03-04 05:29:36 -0700

    tie cart to person not yet logged in

--- routes/products.js
+++ routes/products.js
@@ -15,5 +15,6 @@ router.get("/", async (req, res) => {
 module.exports = router;
 
 // want to be able to show cart and ability to add but retain selections even if the user is not logged in. Then carry the choices over to the logged in user
-//1 tie cart to person not logged in
+//1 tie cart to person not logged in - this logic needs to be able to create active shopping carts and store products in in for un signed in users
+// generate a representation of a shopping cart and assig it a random ID  set that ID inside a cookie and send that the person sending the request.  So it is assinged  to them in their browser. stuff in cart is assinged to that ID which is assinged to that person 
 //2 tie product to cart

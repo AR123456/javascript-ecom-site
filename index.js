da7d719e9b73e8ed870851638912901c121c8182
AR123456 <coloradoz99@gmail.com>
2022-03-08 06:52:46 -0700

    working prototype

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieSession = require('cookie-session');
-const authRouter = require('./routes/admin/auth');
-const adminProductsRouter = require('./routes/admin/products');
-const productsRouter = require('./routes/products');
-const cartsRouter = require('./routes/carts');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
+const authRouter = require("./routes/admin/auth");
+const adminProductsRouter = require("./routes/admin/products");
+const productsRouter = require("./routes/products");
+const cartsRouter = require("./routes/carts");
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cookieSession({
-    keys: ['lkasld235j']
+    keys: ["lkasld235j"],
   })
 );
 app.use(authRouter);
@@ -20,6 +20,8 @@ app.use(productsRouter);
 app.use(adminProductsRouter);
 app.use(cartsRouter);
 
-app.listen(3000, () => {
-  console.log('Listening');
-});
+const port = 3000;
+
+app.listen(port, () =>
+  console.log(`App is listening on http://localhost:${port}`)
+);
